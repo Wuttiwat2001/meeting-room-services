e5ee074d35b22a2a90c20443916aa057c1ee90c5
KhunCaptain <captenlnw_za@hotmail.com>
2024-12-05 01:07:07 +0700

    feat: add User model initialization

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,10 @@
 const { DataTypes } = require("sequelize");
 const { sequelize } = require("../config/database.js");
+const User = require("./User.js");
 
 const db = {};
 
 db.DataTypes = DataTypes;
 db.sequelize = sequelize;
+
+db.User = User(sequelize, DataTypes);
