680e02250c2ed8f87919674eea6dd3b9de7c8942
KhunCaptain <captenlnw_za@hotmail.com>
2024-12-05 01:16:22 +0700

    refactor: add async/await for database synchronization and server start

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const { dbConnection } = require("./config/database.js");
+const db = require("./models/index.js");
 const app = express();
 const PORT = 8081;
 
@@ -21,8 +22,20 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+(async () => {
+  try {
+    await db.sequelize.sync({ force: true });
+    console.log("Database synchronized successfully.");
+  } catch (error) {
+    console.error("Error synchronizing database:", error);
+  }
+})();
 
 app.listen(PORT, async () => {
-  console.log(`Server is running on port ${PORT}`);
-  dbConnection();
+  try {
+    await dbConnection();
+    console.log(`Server is running on port ${PORT}`);
+  } catch (error) {
+    console.error("Error starting server:", error);
+  }
 });
