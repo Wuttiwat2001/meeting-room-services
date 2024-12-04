2d151bb3fdff305dcbe05256149e30896c919084
KhunCaptain <wuttiwat.p@poontana.co.th>
2024-12-04 16:17:02 +0700

    feat: add body-parser and express-session

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
+
+
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
