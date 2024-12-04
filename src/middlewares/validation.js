d687ec5b7637fbf10d908f56479d342d813a3dca
KhunCaptain <captenlnw_za@hotmail.com>
2024-12-05 01:46:05 +0700

    feat: add request validation middleware

---
+++ src/middlewares/validation.js
@@ -0,0 +1,11 @@
+const { validationResult } = require('express-validator');
+
+const validateRequest = (req, res, next) => {
+  const validationErrors = validationResult(req);
+  if (!validationErrors.isEmpty()) {
+    return res.status(400).json({ errors: validationErrors.array() });
+  }
+  next();
+}
+
+module.exports = { validateRequest };
