a5a93bcdcf364c6ad82551267b0ec1a8ebafd801
KhunCaptain <captenlnw_za@hotmail.com>
2024-12-10 00:24:18 +0700

    fix: update validation messages to English

--- src/validators/auth.validator.js
+++ src/validators/auth.validator.js
@@ -1,10 +1,10 @@
 const { body } = require("express-validator");
 
 const register = [
-  body("username", "กรุณากรอกชื่อผู้ใช้ที่มีความยาวอย่างน้อย 3 ตัวอักษร").isString().isLength({ min: 3 }),
-  body("password", "กรุณากรอกรหัสผ่านที่มีความยาวอย่างน้อย 6 ตัวอักษร").isString().isLength({ min: 6 }),
-  body("firstName", "กรุณากรอกชื่อจริง").isString().isLength({ min: 1 }),
-  body("lastName", "กรุณากรอกนามสกุล").isString().isLength({ min: 1 }),
+  body("username", "Username must be at least 3 characters").isString().isLength({ min: 3 }),
+  body("password", "Password must be at least 6 characters").isString().isLength({ min: 6 }),
+  body("firstName", "First name is required").isString().isLength({ min: 1 }),
+  body("lastName", "Last name is required").isString().isLength({ min: 1 }),
 ];
 
 
